8ab163fd2c355525702f2b3f6ee08556de3cc56f
Madele <mtheron@ssi.engineering>
2022-11-25 22:51:09 +0200

    models

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+# The __init__.py file shows that this is a python package --> A collection of modules in a given directory.
---
+++ models/base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the base model"""
+
+
+class BaseModel:
+
+    """BaseModel class from which all other classes will inherit"""
